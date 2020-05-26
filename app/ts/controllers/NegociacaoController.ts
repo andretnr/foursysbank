50a21afdcdbcb88e10dd7de37df6d51fdd5ce186
André Tadeu Nunes Rosa <atnunes@server01.foursys.local>
2020-05-26 16:35:12 -0300

    Adição da função de inserir dados na tabela

--- app/ts/controllers/NegociacaoController.ts
+++ app/ts/controllers/NegociacaoController.ts
@@ -4,11 +4,17 @@ class NegociacaoController {
     private _inputData: HTMLInputElement;
     private _inputQuantidade: HTMLInputElement;
     private _inputValor: HTMLInputElement;
+    private _negociacoes = new Negociacoes();
+    private _negociacoesView = new NegociacoesView('#negociacoesView');
+
+   
 
     constructor(){
         this._inputData = <HTMLInputElement>document.querySelector('#data');
         this._inputQuantidade = <HTMLInputElement>document.querySelector('#quantidade');
         this._inputValor = <HTMLInputElement>document.querySelector('#valor')
+        this._negociacoesView.update(this._negociacoes);
+
     }
 
     adiciona (event: Event){
@@ -18,7 +24,21 @@ class NegociacaoController {
             new Date(this._inputData.value.replace(/-/g, '-')),
             parseInt(this._inputQuantidade.value),
             parseFloat(this._inputValor.value));
+            
+            this._negociacoes.adiciona(negociacao);
+            this._negociacoesView.update(this._negociacoes);
+            
 
-        console.log(negociacao);
+            //testes utilizados durante a codificação
+
+       // console.log(this._negociacao.retornaArray());
+
+      /* this._negociacao.retornaArray().length = 0;
+
+      this._negociacao.retornaArray().forEach(negociacao => {
+            console.log(negociacao.data)
+            console.log(negociacao.quantidade)
+            console.log(negociacao.valor)
+      }); */
     }
 }
